ef862f82acbba35d0b8d8de2cd41bc9948cc0391
Ihor Bachuk <igor.bachuk04@gmail.com>
2026-02-15 16:20:31 +0200

    feat: add timemixin

---
+++ src/tables/__init__.py
@@ -0,0 +1,2 @@
+from src.tables.project import Project  # noqa: F401
+from src.tables.project_place import ProjectPlace  # noqa: F401
---
+++ src/tables/mixins.py
@@ -0,0 +1,16 @@
+import datetime
+
+from sqlalchemy import DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class TimeMixin:
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    updated_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
